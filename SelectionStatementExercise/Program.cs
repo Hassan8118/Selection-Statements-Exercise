af0991717afb5e10a78f4537186eef62736f172c
hassan8118 <mujtabahassan1234567@gmail.com>
2023-04-12 13:00:53 -0400

    SelectionStatements1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            var favNumber = r.Next(1, 10);
+            Console.WriteLine("pick a number from 1-10");
+            var userInput = int.Parse(Console.ReadLine());
+
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine($"{userInput} is too low");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine($"{userInput} is too high");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
         }
     }
 }
